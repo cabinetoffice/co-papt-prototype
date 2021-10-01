fefd9c4644023aab6b93f823aa82c9c889f9c832
briananddigital <90381147+BrianMilmanXansium@users.noreply.github.com>
2021-10-01 09:13:16 +0100

    Sprint 22 creation

--- app/routes.js
+++ app/routes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const v1Routes = require('./routes/v1');
+const v2Routes = require('./routes/v2');
 
-
-router.use('/', v1Routes);
+router.use('/', v2Routes);
 
 
 module.exports = router;
