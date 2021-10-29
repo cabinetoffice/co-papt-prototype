a5d572731acebc9e5ab51b12be44b5943f14f764
briananddigital <90381147+BrianMilmanXansium@users.noreply.github.com>
2021-10-29 09:17:06 +0100

    Created ver 4

--- app/routes.js
+++ app/routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const v1Routes = require('./routes/v1');
 const v2Routes = require('./routes/v2');
 const v3Routes = require('./routes/v3');
+const v4Routes = require('./routes/v4');
 
 router.use('/', v1Routes);
 router.use('/', v2Routes);
 router.use('/', v3Routes);
+router.use('/', v4Routes);
 
 module.exports = router;
