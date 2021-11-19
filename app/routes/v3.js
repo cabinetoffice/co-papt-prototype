5dc98e93d7fcf6c9e2a2fb0e52db59d53bc4670b
briananddigital <90381147+BrianMilmanXansium@users.noreply.github.com>
2021-11-19 14:46:40 +0000

    cleanded up ver sections

--- app/routes/v3.js
+++ app/routes/v3.js
@@ -3,80 +3,9 @@ const router = express.Router()
 
 const verNum = 3
 
-//EXAMPLE ROUTES START
 
-// Add your routes here - above the module.exports line
-router.post(`/v${verNum}/teach-eligible-branch`, function (req, res) {
-  const checkChoice = req.session.data["eligible-subject"]
 
-  if (checkChoice === "yes") {
-    res.redirect(`/v${verNum}/current-school`)
-  } else {
-    res.redirect(`/v${verNum}/teach-ineligible`)
-  }
-})
 
-router.post(`/v${verNum}/school-eligible-branch`, function (req, res) {
-  const checkSchool = req.session.data["eligible-school"]
-
-  if (
-    checkSchool === "The Hathershaw College" ||
-    checkSchool === "KEVI" ||
-    checkSchool === "Queen Elizabeths Grammar School"
-  ) {
-    res.redirect(`/v${verNum}/itt-subject`)
-  } else {
-    res.redirect(`/v${verNum}/school-ineligible`)
-  }
-})
-
-//CHECKBOX EXAMPLE
-
-router.post(`/v${verNum}/tslr/tslr-subject-branch`, function (req, res) {
-  let subjectsTaught = req.session.data["subjectsTaught"] // as the checkboxes are a group you get all values as an array
-  let uncheckedCount = 0
-  let unselectedCheckboxesExist = true
-
-  // if Array is undefined, unselectedCheckboxesExist = true
-  if (subjectsTaught === undefined) {
-    unselectedCheckboxesExist = true
-  } else {
-    // count the number of unchecked items
-    subjectsTaught.forEach((element) => {
-      if (element === "_unchecked") {
-        uncheckedCount++
-      }
-    })
-    // if the unchecked count is the same as the length of the array then they are all unchecked
-    if (uncheckedCount === subjectsTaught.length) {
-      unselectedCheckboxesExist = true
-    } else {
-      unselectedCheckboxesExist = false
-    }
-  }
-
-  // know we know, send the user to the correct page
-  if (unselectedCheckboxesExist) {
-    res.redirect(`/v${verNum}/tslr/tslr-ineligible`)
-  } else {
-    res.redirect(`/v${verNum}/tslr/tslr-still-employed`)
-  }
-})
-
-//CHECKBOX EXAMPLE
-
-router.post(`/v${verNum}/check-answers-part-one-branch`, function (req, res) {
-  const checkItt = req.session.data["eligible-year"]
-  const checkSup = req.session.data["supply-teacher"]
-
-  if (checkItt === "None of the above") {
-    res.redirect(`/v${verNum}/generic-ineligible`)
-  } else if (checkSup === "No" && checkItt !== "None of the above") {
-    res.redirect(`/v${verNum}/check-answers-part-one`)
-  } else res.redirect(`/v${verNum}/check-answers-part-one-alt`)
-})
-
-//EXAMPLE ROUTES END
 
 //UPLOAD A NEW CV start
 
